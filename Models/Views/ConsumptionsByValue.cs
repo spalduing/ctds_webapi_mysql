5d4c5c0bf7d2d31cc2a0b6413866f4a3f763195e
mdiazc <miguelangel.diaz.kommit@gmail.com>
2022-11-14 20:13:24 -0500

    -Changed: stable queries

--- Models/Views/ConsumptionsByValue.cs
+++ Models/Views/ConsumptionsByValue.cs
@@ -4,6 +4,6 @@ public class ConsumptionsByValue
     public string Name {get;set;}
     public string LastName {get;set;}
     public double CustomerConsumption {get;set;}
-    public DateTime CreatedAt {get;set;}
+    // public DateTime CreatedAt {get;set;}
 
 }
